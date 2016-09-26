5c9d718a2f1508218838042f2f2a2d555e9ed970
MatheusBessa <mb.matheusbessa@gmail.com>
2016-09-26 00:04:32 -0300

    Update form_entrevista.php

--- src/html/form_entrevista.php
+++ src/html/form_entrevista.php
@@ -1,8 +1,30 @@
 <html>
   <head>
+    <!-- Inclusão css bootstrap -->
+    <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+    <!-- Inclusão javascript bootstrap -->
+    <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
   </head>
   
 <body>
-
+  <div class="container">
+    
+    <div class="form-group">
+      <label for="questio">Pergunta:</label>
+      <input type="text" class="form-control" id="questio">
+    </div>
+    
+    <div class="form-group">
+      <label for="answer">Resposta:</label>
+      <textarea class="form-control" rows="5" id="answer"></textarea>
+    </div>
+    
+    
+    <div class="form-group">
+      <label for="comment">Comentários:</label>
+      <textarea class="form-control" rows="5" id="comment"></textarea>
+    </div> 
+  </div>
 </body>
 </html>
