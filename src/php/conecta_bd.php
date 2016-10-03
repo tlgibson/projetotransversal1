872004e65d36a6ea9fd05d733b9d20e4af02f8f8
MatheusBessa <mb.matheusbessa@gmail.com>
2016-10-03 14:26:34 -0300

    Correção do construtor da classe

--- src/php/conecta_bd.php
+++ src/php/conecta_bd.php
@@ -4,7 +4,7 @@ class conecta_bd{
 
 	var $conn=NULL;
 
-public function conecta_bd(){
+public function __construct(){
 
 	$servername = "localhost";
 	$username = "uiba_user";
@@ -15,10 +15,10 @@ public function conecta_bd(){
     		
 		// set the PDO error mode to exception
     		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    		//echo "Connected successfully";
+
     	
 	}catch(PDOException $e){
-    		//echo "Connection failed: " . $e->getMessage();
+    		echo "Connection failed: " . $e->getMessage();
     	}
 
 }
