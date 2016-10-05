a96671c597869b01472abdaa8a0f4ce2b36d7313
Mariana <mariana13mpc@gmail.com>
2016-10-05 14:05:03 -0300

    Login

---
+++ Login.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Bootstrap - Primeiros Passos</title><
+<!-- Incluindo o CSS do Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<!-- Incluindo o JavaScript do Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<form>
+  <label for="matr">Matricula</label>
+  <input id="matr" type="text" placeholder="Preencha com a Matricula..." name="matr />
+  <label for="senha">Senha</label>
+  <input id="senha" type="password" placeholder="Digite a sua senha..."  name="senha"/>
+  <label>
+    <input type="checkbox" /> Lembrar senha
+  </label>
+  <button type="submit">Acessar</button>
+</form>
+</body>
+</html>
