771af08d1ccc2a8d46082e9e257ed83137a74e0c
MatheusBessa <mb.matheusbessa@gmail.com>
2016-10-04 22:01:40 -0300

    modelo cadastro em php

---
+++ src/php/model_user.php
@@ -0,0 +1,103 @@
+<?php 
+include_once("conecta_bd.php"); // Arquivo padrão para conexão
+include_once("user.php");
+
+
+
+
+class model_user{
+
+
+	private $conn;
+	private $user;	
+	
+public function __construct($user){
+	
+	$cb = new conecta_bd(); 
+	$this->user = $user;
+	$this->conn = $cb->get_connection();
+}
+
+
+
+public function cadastra_usuario($user){
+
+	$sql = 'INSERT INTO usuarios( nome, email, matricula, cpf, data_nascimento, telefone, nome_mae, tipo_user, senha)
+	        VALUES (:nome,:email,:matricula,:cpf,:data_nasc,:telefone,:nome_mae,:tipo_user,:senha)';
+	$stmt = $this->conn->prepare($sql);
+	
+	$stmt->bindValue(':nome', this->user->getNome());
+	$stmt->bindValue(':email', this->user->getEmail());
+	$stmt->bindValue(':matricula', this->user->getMatricula());
+	$stmt->bindValue(':cpf', this->user->getCpf()); 
+	$stmt->bindValue(':data_nasc', this->user->getData_nascimento());
+	$stmt->bindValue(':telefone', this->user->getTelefone());
+	$stmt->bindValue(':nome_mae', this->user->getNome_mae());
+	$stmt->bindValue(':tipo_user', this->user->getTipo_user());
+	$stmt->bindValue(':senha', this->user->getSenha());
+
+	$stmt->execute();
+
+}
+
+
+public function atualiza_usuario($user){
+
+	$sql = 'UPDATE usuarios
+			SET nome = :nome, 
+			    email = :email,
+			    matricula = :matricula,
+			    cpf = :cpf,
+			    data_nascimento = :data_nasc,
+			    telefone = :telefone,
+			    nome_mae = :nome_mae,
+			    tipo_user= :tipo_user,
+			    senha = :senha
+			WHERE id = :id';
+
+	$stmt = $this->conn->prepare($sql);
+
+	$stmt->bindValue(':nome', this->user->getNome());
+	$stmt->bindValue(':email', this->user->getEmail());
+	$stmt->bindValue(':matricula', this->user->getMatricula());
+	$stmt->bindValue(':cpf', this->user->getCpf()); 
+	$stmt->bindValue(':data_nasc', this->user->getData_nascimento());
+	$stmt->bindValue(':telefone', this->user->getTelefone());
+	$stmt->bindValue(':nome_mae', this->user->getNome_mae());
+	$stmt->bindValue(':tipo_user', this->user->getTipo_user());
+	$stmt->bindValue(':senha', this->user->getSenha());
+	$stmt->bindValue(':id', this->user->getId());
+
+
+	$stmt->execute();
+}//Fim atualiza_usuario();
+
+public function remover_usuario($id){
+	$sql = 'DELETE FROM usuarios WHERE id=:id';
+	$stmt = $this->conn->prepare($sql);
+	$stmt->bindValue(':id',$id);
+	$stmt->execute();
+}//Fim remover_usuario()
+
+
+public function seleciona_usuario($id){
+	$sql = 'SELECT * FROM usuarios WHERE id = :id'; 
+	$stmt = $this->conn->prepare($sql);
+	$stmt->bindValue(':id',$id);
+	$stmt->execute();
+}//Fim seleciona_usuario()
+
+public function lista_usuarios(){
+	$sql = 'SELECT * FROM usuarios'; 
+	$stmt = $this->conn->prepare($sql);
+	$stmt->bindValue(':id',$id);
+	$stmt->execute();
+
+}
+
+
+}//Fim classe
+
+
+
+?>
---
+++ src/php/user.php
@@ -0,0 +1,110 @@
+<?php
+class user{
+
+	
+	private $nome;
+	private $email;
+	private $matricula; 
+	private $cpf; 
+	private $data_nascimento; 
+	private $telefone; 
+	private $nome_mae; 
+	private $tipo_user; 
+	private $senha;
+	private $id
+
+function __construct(){
+
+
+}
+
+public function getId(){
+	return $this->id;
+}
+
+public function getNome(){
+	return $this->nome;
+}
+
+public function getEmail(){
+	return $this->email;
+}
+
+public function getMatricula(){
+	return $this->matricula;
+}
+
+
+public function getCpf(){
+	return $this->cpf;
+}
+
+public function getData_nascimento(){
+	return $this->data_nascimento;
+}
+
+
+public function getTelefone(){
+	return $this->telefone;
+}
+
+public function getNome_mae(){
+	return $this->nome_mae;
+}
+
+public function getTipo_user(){
+	return $this->tipo_user;
+}
+
+public function getSenha(){
+	return $this->senha;
+}
+
+
+public function setId($id){
+	$this->id = $id;
+}
+
+public function setNome($nome){
+	$this->nome = $nome;
+}
+
+public function setEmail($email){
+	$this->email = $email;
+}
+
+public function setMatricula($matricula){
+	$this->matricula = $matricula;
+}
+
+
+public function setCpf($cpf){
+	$this->cpf = $cpf;
+}
+
+public function setData_nascimento($data_nascimento){
+	$this->data_nascimento = $data_nascimento;
+}
+
+
+public function setTelefone($telefone){
+	$this->telefone = $data_nascimento;
+}
+
+public function setNome_mae($nome_mae){
+	$this->nome_mae = $nome_mae;
+}
+
+public function setTipo_user($tipo_user){
+	$this->tipo_user = $tipo_user;
+}
+
+public function setSenha($senha){
+	$this->senha = $senha;
+}
+
+
+}//Fim Classe
+
+
+?>
