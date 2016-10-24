94461c96f232257cfbdf570eed867fd7e40da4b2
MatheusBessa <mb.matheusbessa@gmail.com>
2016-10-24 15:14:07 -0200

    login uiba

--- src/php/control_login.php
+++ src/php/control_login.php
@@ -4,17 +4,23 @@ include("model_user.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	//Coloca os POTS do form_login em variaveis e as formata
 	$email = test_input($_POST['email']);
-	$senha = test_input($_POST['senha']);
+	$senha = sha1(test_input($_POST['senha']));
+
 
 	$modelUser = new model_user();
 	$result = $modelUser->valida_login($email);
 
-	if(valida_login($result,$senha)){
+	$b = valida_login($result,$senha);
+
+	
+	if(valida_login($result,$senha) === true){
 		
-		header("Location: http://localhost/projetotransversal1/src/html/login_success.php");exit;
+
+
+		header("Location: ../html/home.php");exit;
 	}else{
 		
-		header("Location: http://localhost/projetotransversal1/src/html/login_fail.php");exit;
+		header("Location: ../html/login_fail.php");exit;
 	}
 	
 }
@@ -23,7 +29,13 @@ function valida_login($result,$senha){
 
 	if(count($result)>0){
 		foreach ($result as $user) {
-			if($user['senha'] == $senha){
+			
+			if($user['senha'] === $senha){
+				session_start();
+				$_SESSION["usuario_logado"] = true;
+				$_SESSION["usuario"] = $user["nome"];
+				$_SESSION["id_user"] = $user["id"];
+				$_SESSION["perfil"] = $user["tipo_user"];
 				return true;
 			}else{
 				return false;
